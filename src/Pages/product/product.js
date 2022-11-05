6bc4dcb47f3884bc74ae0a0aaebb8e95fc6bbac8
Suleman Ahmed <48311202+Suleman3015@users.noreply.github.com>
2022-11-05 23:48:04 +0500

    pushes

--- src/Pages/product/product.js
+++ src/Pages/product/product.js
@@ -7,14 +7,17 @@ import hero5 from "../../content/hero33.jpg";
 import hero3 from "../../content/hero3.jpg";
 import hero4 from "../../content/hero4.jpg";
 import Rating from "@mui/material/Rating";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function Product() {
+  const matches = useMediaQuery("(max-width:700px)");
+  const matchesPro = useMediaQuery("(max-width:1000px)");
   return (
     <div className="productDetail">
       <div className="productDetailSlider">
         {/* for slider */}
         <HeroSlider
-          height="70vh"
+                  height={matches ? "40vh":matchesPro ? "60vh": "70vh"}
           //   accessability={{
           //     shouldDisplayButtons: false,
           //     orientation: "vertical",
@@ -22,7 +25,7 @@ function Product() {
           //   autoplay
           controller={{
             initialSlide: 1,
-            slidingDuration: 500,
+            slidingDuration: 300,
             slidingDelay: 100,
             onSliding: (nextSlide) =>
               console.debug("onSliding(nextSlide): ", nextSlide),
@@ -44,7 +47,7 @@ function Product() {
       </Overlay> */}
 
           <Slide
-            shouldRenderMask
+            // shouldRenderMask
             label="Hallstatt - Austria"
             background={{
               backgroundImage: hero1,
@@ -115,6 +118,17 @@ function Product() {
           value={5}
           readOnly
         />
+        <p className="productDes">
+          Order the best furniture online in Pakistan. The Vogue Wood
+          manufactures the classic highest-quality,luxurious and modern
+          furniture for a unique touch of class and elegance to home interiors.
+          Ranging from your bedroom to the kitchen, products from various
+          furniture categories are available for a unique touch of class and elegance to home interiors.
+          Ranging from your bedroom to the kitchen, products from various
+          furniture categories are available 
+        </p>
+
+        <button className="productButton"> For Queries</button>
         {/* </div> */}
       </div>
     </div>
