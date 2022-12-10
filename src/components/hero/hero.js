03da17d25a8e12e2aa3fe4aefcbce0d7e16f76aa
Suleman Ahmed <48311202+Suleman3015@users.noreply.github.com>
2022-12-10 16:10:07 +0500

    pushes

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,15 +1,86 @@
-import HeroSlider, { Slide, Overlay } from "hero-slider";
-import hero1 from "../../content/hero1.jpg";
-import hero2 from "../../content/hero2.jpg";
-import hero5 from "../../content/hero33.jpg";
-import hero3 from "../../content/hero3.jpg";
-import hero4 from "../../content/hero4.jpg";
+// import HeroSlider, { Slide, Overlay } from "hero-slider";
+import HeroSlider, { Slide, Overlay} from "hero-slider";
+// import hero1 from "../../content/hero1.jpg";
+// import hero2 from "../../content/hero2.jpg";
+// import hero5 from "../../content/hero33.jpg";
+// import hero3 from "../../content/hero3.jpg";
+// import Hero4 from "../../content/hero4.jpg";
+import hero from "../../content/pexx.jpg"
 import Wrapper from "./wrapper";
 import Title from "./title";
 import Subtitle from "./subtitle";
 
 function Hero() {
   return (
+    // <HeroSlider
+    //   height="100vh"
+    //   accessability={{
+    //     shouldDisplayButtons: false,
+    //     orientation: "vertical",
+    //   }}
+    //   autoplay
+    //   controller={{
+    //     slidingDuration: 600,
+    //     slidingDelay:50,
+    //   }}
+    //   style={{
+    //     position: "sticky",
+    //     marginBottom: "40px",
+    //   }}
+    // >
+    //   <Overlay>
+    //     <Wrapper>
+    //       <Title>The Vogue Wood</Title>
+    //       <Subtitle>We Are Artist</Subtitle>
+    //     </Wrapper>
+    //   </Overlay>
+
+    //   <Slide
+    //     shouldRenderMask
+    //     label="Hallstatt - Austria"
+    //     background={{
+    //       backgroundImage: hero1,
+    //       // backgroundAnimation: 'zoom',
+    //     }}
+    //   />
+
+    //   <Slide
+    //     shouldRenderMask
+    //     label="Hvitserkur - Iceland"
+    //     background={{
+    //       backgroundImage: hero2,
+    //       // backgroundAnimation: 'zoom'
+    //     }}
+    //   />
+
+    //   <Slide
+    //     shouldRenderMask
+    //     label="Moraine Lake - Canada"
+    //     background={{
+    //       backgroundImage: hero5,
+    //       // backgroundAnimation: 'zoom'
+    //     }}
+    //   />
+
+    //   <Slide
+    //     shouldRenderMask
+    //     label="Jacksonville - USA"
+    //     background={{
+    //       backgroundImage: hero3,
+    //       // backgroundAnimation: 'zoom'
+    //     }}
+    //   />
+
+    //   <Slide
+    //     shouldRenderMask
+    //     label="Moraine Lake - Canada"
+    //     background={{
+    //       backgroundImage: hero4,
+    //       // backgroundAnimation: 'zoom'
+    //     }}
+    //   />
+
+    // </HeroSlider>
     <HeroSlider
       height="100vh"
       accessability={{
@@ -18,8 +89,8 @@ function Hero() {
       }}
       autoplay
       controller={{
-        slidingDuration: 600,
-        slidingDelay:50,
+        slidingDuration: 400,
+        slidingDelay: 50,
       }}
       style={{
         position: "sticky",
@@ -35,58 +106,44 @@ function Hero() {
 
       <Slide
         shouldRenderMask
-        label="Hallstatt - Austria"
+        label="Giau Pass - Italy"
         background={{
-          backgroundImage: hero1,
-          // backgroundAnimation: 'zoom',
+          backgroundImageSrc: hero,
         }}
       />
 
       <Slide
         shouldRenderMask
-        label="Hvitserkur - Iceland"
+        label="Bogliasco - Italy"
         background={{
-          backgroundImage: hero2,
-          // backgroundAnimation: 'zoom'
+          backgroundImageSrc: hero,
         }}
       />
 
       <Slide
         shouldRenderMask
-        label="Moraine Lake - Canada"
+        label="County Clare - Ireland"
         background={{
-          backgroundImage: hero5,
-          // backgroundAnimation: 'zoom'
+          backgroundImageSrc: hero,
         }}
       />
 
       <Slide
         shouldRenderMask
-        label="Jacksonville - USA"
+        label="Crater Rock, OR - United States"
         background={{
-          backgroundImage: hero3,
-          // backgroundAnimation: 'zoom'
+          backgroundImageSrc: hero,
         }}
       />
 
       <Slide
         shouldRenderMask
-        label="Moraine Lake - Canada"
+        label="Crater Rock, OR - United States"
         background={{
-          backgroundImage: hero4,
-          // backgroundAnimation: 'zoom'
+          backgroundImageSrc: hero,
         }}
       />
-
-      {/* <ButtonsNav
-        isNullAfterThreshold
-        position={{
-          top: 0,
-          left: '50%',
-          transform: 'translateX(-50%)'
-        }}
-      />
-      <Nav /> */}
+      {/* <Nav /> */}
     </HeroSlider>
   );
 }
